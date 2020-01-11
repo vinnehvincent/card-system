4ed9c7aff80fac98e85d654f66c97176ca48fa94
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2020-01-11 20:35:47 +0200

    Added: exception to CardHolder

--- src/test/java/model/CardHolderTest.java
+++ src/test/java/model/CardHolderTest.java
@@ -10,14 +10,6 @@ public class CardHolderTest {
         CardHolder cardHolder = new CardHolder();
         assertTrue(cardHolder.getCards().size() > 0);
     }
-//    @Test
-//    public void aCardHolderMustHaveAMaximumOf1PrimaryCardAndUpTo6SecondaryCards() throws Exception {
-//        CardHolder cardHolder = new CardHolder();
-//        for (int i = 0; i < 7; i++)
-//            cardHolder.addSecondaryCard();
-//        assertNotNull(cardHolder.getPrimaryCard());
-//        assertEquals(6, cardHolder.getSecondaryCards().size());
-//    }
     @Test(expected = Exception.class)
     public void aCardHolderCannotHaveNoMoreThan1PrimaryCardAnd6SecondaryCards() throws Exception {
         CardHolder cardHolder = new CardHolder();

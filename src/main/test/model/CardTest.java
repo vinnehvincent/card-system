b1b868d10d8bbd646898b74bfc7282601114a938
Vincent Tshabalala <vincent.b.tshabalala@gmail.com>
2020-01-11 05:49:14 +0200

    Refactored: static imports for enums

--- src/main/test/model/CardTest.java
+++ src/main/test/model/CardTest.java
@@ -1,28 +1,28 @@
 package model;
 
-import model.Card;
-import model.CardStatus;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static model.CardStatus.*;
 
 public class CardTest {
     @Test
     public void aNewCardMustBeInactive(){
         Card card = new Card();
-        Assert.assertEquals(CardStatus.INACTIVE, card.getStatus());
+        Assert.assertEquals(INACTIVE, card.getStatus());
     }
     @Test
     public void onceActivatedACardWillNeverBeSetToInactiveAgain(){
         Card card = new Card();
-        card.setStatus(CardStatus.ACTIVE);
-        card.setStatus(CardStatus.INACTIVE);
-        Assert.assertEquals(CardStatus.ACTIVE,card.getStatus());
+        card.setStatus(ACTIVE);
+        card.setStatus(INACTIVE);
+        Assert.assertEquals(ACTIVE,card.getStatus());
     }
     @Test
     public void onceCLOSEDaCardStatusCannotBeChangedAgain(){
         Card card = new Card();
-        card.setStatus(CardStatus.CLOSED);
-        card.setStatus(CardStatus.ACTIVE);
-        Assert.assertEquals(CardStatus.CLOSED,card.getStatus());
+        card.setStatus(CLOSED);
+        card.setStatus(ACTIVE);
+        Assert.assertEquals(CLOSED,card.getStatus());
     }
 }
